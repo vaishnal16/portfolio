a674255f6a4b2022a8eaea44260bcbdbe6b398ea
Vaishnal Mali <vaishnal16305@gmail.com>
2025-05-31 15:37:30 +0530

    endpoint changes

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -26,7 +26,7 @@ const ContactSection = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/contacts', {
+      const response = await fetch('https://portfolio-backend-q2bv.onrender.com/api/contacts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
